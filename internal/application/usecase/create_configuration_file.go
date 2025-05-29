9ed292be42c123b5d6463439065f16e127ee5774
Yuri Oliveira <yusadeol@gmail.com>
2025-05-29 19:27:32 -0300

    feat: add use case for creating configuration file
    - Create create_configuration_file.go for configuration file creation logic
    - Refactor init command to use the new use case
    - Remove duplicate initialization logic from init command

---
+++ internal/application/usecase/create_configuration_file.go
@@ -0,0 +1,72 @@
+package usecase
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/yusadeol/go-commit/internal/Domain/vo"
+)
+
+var ErrConfigurationAlreadyExists = errors.New("configuration already exists")
+
+var defaultConfiguration = vo.Configuration{
+	DefaultAIProvider: "openai",
+	DefaultLanguage:   "en_US",
+	AIProviders: map[string]vo.AIProvider{
+		"openai": {
+			ID:     "openai",
+			APIKey: "",
+			Models: []string{
+				"gpt-4.1",
+			},
+			DefaultModel: "gpt-4.1",
+		},
+	},
+	Languages: map[string]vo.Language{
+		"en_US": {
+			ID:          "en_US",
+			DisplayName: "English (United States)",
+		},
+		"pt_BR": {
+			ID:          "pt_BR",
+			DisplayName: "Portuguese (Brazil)",
+		},
+		"es_ES": {
+			ID:          "es_ES",
+			DisplayName: "Spanish (Spain)",
+		},
+	},
+}
+
+type CreateConfigurationFile struct{}
+
+func NewCreateConfigurationFile() *CreateConfigurationFile {
+	return &CreateConfigurationFile{}
+}
+
+func (c *CreateConfigurationFile) Execute(input *CreateConfigurationFileInput) error {
+	_, err := os.Stat(input.ConfigurationDirPath)
+	if err != nil {
+		return err
+	}
+	configurationMarshal, err := json.MarshalIndent(defaultConfiguration, "", "    ")
+	if err != nil {
+		return err
+	}
+	configurationFilePath := filepath.Join(input.ConfigurationDirPath, "commit.json")
+	_, err = os.Stat(configurationFilePath)
+	if err == nil {
+		return ErrConfigurationAlreadyExists
+	}
+	err = os.WriteFile(configurationFilePath, configurationMarshal, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type CreateConfigurationFileInput struct {
+	ConfigurationDirPath string
+}
--- internal/interface/cli/command/init.go
+++ internal/interface/cli/command/init.go
@@ -1,42 +1,14 @@
 package command
 
 import (
-	"encoding/json"
+	"errors"
 	"os"
 	"path/filepath"
 
 	"github.com/yusadeol/go-commit/internal/Domain/vo"
+	"github.com/yusadeol/go-commit/internal/application/usecase"
 	"github.com/yusadeol/go-commit/internal/interface/cli"
 )
-
-var initConfiguration = vo.Configuration{
-	DefaultAIProvider: "openai",
-	DefaultLanguage:   "en_US",
-	AIProviders: map[string]vo.AIProvider{
-		"openai": {
-			ID:     "openai",
-			APIKey: "",
-			Models: []string{
-				"gpt-4.1",
-			},
-			DefaultModel: "gpt-4.1",
-		},
-	},
-	Languages: map[string]vo.Language{
-		"en_US": {
-			ID:          "en_US",
-			DisplayName: "English (United States)",
-		},
-		"pt_BR": {
-			ID:          "pt_BR",
-			DisplayName: "Portuguese (Brazil)",
-		},
-		"es_ES": {
-			ID:          "es_ES",
-			DisplayName: "Spanish (Spain)",
-		},
-	},
-}
 
 type Init struct{}
 
@@ -63,22 +35,15 @@ func (g *Init) Execute(input *cli.CommandInput) (*cli.Result, error) {
 		return nil, err
 	}
 	configurationDirPath := filepath.Join(homeDirPath, ".config")
-	_, err = os.Stat(configurationDirPath)
-	if err != nil {
-		return nil, err
-	}
-	configurationMarshal, err := json.MarshalIndent(initConfiguration, "", "    ")
-	if err != nil {
-		return nil, err
-	}
-	configurationFilePath := filepath.Join(configurationDirPath, "commit.json")
-	_, err = os.Stat(configurationFilePath)
-	if err == nil {
+	createConfigurationFile := usecase.NewCreateConfigurationFile()
+	err = createConfigurationFile.Execute(&usecase.CreateConfigurationFileInput{
+		ConfigurationDirPath: configurationDirPath,
+	})
+	if errors.Is(err, usecase.ErrConfigurationAlreadyExists) {
 		result.ExitCode = vo.ExitCodeError
 		result.Message = vo.NewMarkupText("<info>configuration file already exists</info>")
 		return result, nil
 	}
-	err = os.WriteFile(configurationFilePath, configurationMarshal, 0644)
 	if err != nil {
 		return nil, err
 	}
